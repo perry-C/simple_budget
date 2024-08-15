e56f397031e98842424ba7e1889ebe07f3316621
Perry Chen <2558544545qwerty@gmail.com>
2024-08-15 14:30:25 +0100

    Feat: created user input page for budget calculation

--- src/app/budget/page.jsx
+++ src/app/budget/page.jsx
@@ -1,26 +1,56 @@
-'use client'
+'use client';
 
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
 import { useState } from 'react';
+
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card';
+
 const BudgetCalculatorPage = () => {
-    const [submitted, setSubmitted] = useState(false)
+    const [submitted, setSubmitted] = useState(false);
     return (
-        <div id='budget-page-container' className='flex justify-center items-center' style={{minHeight: '100vh'}}>
-            <div id='user-input-form' className='flex flex-col space-y-2'> 
-            <h1 className=''>Budget Calculator</h1>
-            <form>
-                <Input id='Income' placeholder='Monthly income'/>
-                <br/>
-                <Input id='scheduled-spending' placeholder='Scheduled spending'/>
-                <br/>
-                
-            </form>
-            <Button>Submit</Button>
+        <div
+            id="budget-page-container"
+            className="flex justify-center items-center"
+            style={{ minHeight: '100vh' }}
+        >
+            <div id="user-input-form">
+                <Card>
+                    <CardHeader>
+                        <CardTitle>Budget Calculator</CardTitle>
+                        <CardDescription>
+                            Input your spending situation to get advise on
+                            budget
+                        </CardDescription>
+                    </CardHeader>
+                    <CardContent className='flex flex-col'>
+                        <form>
+                            <Input id="income" placeholder="Monthly income" />
+                            <br />
+                            <Input
+                                id="scheduled-spending"
+                                placeholder="Scheduled spending"
+                            />
+                            <br />
+                            <Input id="debt" placeholder="Debt" />
+                            <br />
+                        </form>
+                        <Button>Submit</Button>
+                    </CardContent>
+                    <CardFooter>
+                        <p className='text-secondary ml-auto'>© 2024 ... Inc.</p>
+                    </CardFooter>
+                </Card>
             </div>
         </div>
-        
-    )
-}
+    );
+};
 
-export default BudgetCalculatorPage
+export default BudgetCalculatorPage;
